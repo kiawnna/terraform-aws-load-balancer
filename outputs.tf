176196b9cb3edb6e78808723abf94f2601fc36f4
kiawnna <kiawnna@gmail.com>
2022-01-08 18:34:31 -0600

    updated listener output

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "lb_zone_id" {
   value = aws_lb.ALB.zone_id
 }
 output "listener_443_arn" {
-  value = aws_lb_listener.https.*.arn
+  value = var.create_listeners == true ? aws_lb_listener.https.arn : null
 }
 output "load_balancer_arn_suffix" {
   value = aws_lb.ALB.arn_suffix
